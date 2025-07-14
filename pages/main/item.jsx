67e979e7d4025a93cdde502a91036dad05b47019
parsa ardalan <parsardlan2112@gmail.com>
2025-07-14 23:27:43 +0330

    style: shadow added. bg color removed

--- pages/main/item.jsx
+++ pages/main/item.jsx
@@ -5,14 +5,14 @@ export default function MenuPart(props) {
 
     return (
 
-        <Link href={`/items/${props.address}`} className="col-span-6 rounded-2xl backdrop-blur-2xl backdrop-brightness-45 h-full cursor-pointer hover:scale-105 transition-transform duration-200 block">
+        <Link href={`/items/${props.address}`} className="col-span-6 rounded-2xl backdrop-blur-2xl shadow-lg backdrop-brightness-75 shadow-black h-full cursor-pointer hover:scale-105 transition-transform duration-200 block">
 
             <div>
 
                 <Image src={props?.image} className="mx-auto -mt-8" alt={props?.name} width={80} height={80} />
 
                 <h1 className="text-center text-lg w-full mt-2 text-white"> {props.name} </h1>
-                <p className="text-stone-500 text-center text-xs mb-4 md:text-sm"> click to see more </p>
+                <p className="text-white text-center text-xs mb-4 md:text-sm"> click to see more </p>
             </div>
 
         </Link>
